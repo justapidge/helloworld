aff9bc66688063a6a92df7b7012957cbbeb4e852
night <1924440305@qq.com>
2023-11-26 09:53:01 +0800

    附加件

---
+++ JDBCLogin/src/main/java/com/iweb/test5/TTtttt.java
@@ -0,0 +1,11 @@
+package com.iweb.test5;
+
+/**
+ * @author wmn
+ * @date 2023/11/26 9:50
+ */
+public class TTtttt {
+    public static void main(String[] args) {
+        System.out.println("sufawjvadv");
+    }
+}
